0d337b23e5f6340fcf9c532a7b314f82974eb30a
Pablo Park <ppark@est.frba.utn.edu.ar>
2021-08-05 11:43:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ function onMouseMove(event){
     console.log(x,y);
 }
 
+function onMouseDown(event)
+{
+    console.log(event);
+}
+
 if (canvas){
     canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
 }
